95a2d11b334c8ba851e4aff1d66a9edd2f4f3cb4
Gilad <giladmanor01@gmail.com>
2014-11-07 19:17:01 +0200

    turn mechanics

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var merge = require('merge');
+
+var calculateNextTurn = function(gameObject){
+	var current = gameObject["turn"];
+	var players = Object.keys(gameObject);
+	console.log("turn "+ current+" of "+ players.length);
+	players.splice(players.indexOf("turn"), 1);
+	players.sort();
+	var i = players.indexOf(current);
+	i = (i+1)%players.length;
+	return players[i];
+};
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
@@ -18,11 +30,16 @@ io.on('connection', function(socket) {
 
 	socket.on('register', function(code) {
 		console.log('register: ', code);
+		global[code] = global[code] || {turn:""};
 		socket.on(code, function(msg) {
-			console.log(code, msg);
-			io.emit(code, msg);
+			console.log(msg);
+			global[code] = merge.recursive(true, global[code], msg);
+			console.log(code, global[code]);
+			global[code]["turn"] = calculateNextTurn(global[code]);
+			console.log(code, global[code]);
+			io.emit(code, global[code]);
 		});
-		io.emit(code, "Welcome!");
+		io.emit(code, global[code]);
 	});
 
 });
