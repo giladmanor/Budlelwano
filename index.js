afb05eaeb408f6cc1284f2eaace10c9ff39750a1
Gilad <giladmanor01@gmail.com>
2014-11-09 13:19:24 +0200

    event mechanics

--- index.js
+++ index.js
@@ -3,15 +3,43 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var merge = require('merge');
 
-var calculateNextTurn = function(gameObject){
+var calculateNextTurn = function(gameObject) {
 	var current = gameObject["turn"];
-	var players = Object.keys(gameObject);
-	console.log("turn "+ current+" of "+ players.length);
-	players.splice(players.indexOf("turn"), 1);
+	var players = Object.keys(gameObject.players);
+	console.log("turn " + current + " of " + players.length);
+
 	players.sort();
 	var i = players.indexOf(current);
-	i = (i+1)%players.length;
+	i = (i + 1) % players.length;
 	return players[i];
+};
+
+var calculateEvent = function(gameObject, lastPlayer) {
+	var lastPlayerKey = Object.keys(lastPlayer)[0];
+	var loc = lastPlayer[lastPlayerKey].location;
+	var players = Object.keys(gameObject.players);
+	var event = {
+		players : []
+	};
+	var candidates = [];
+	var locsum = 0;
+	players.forEach(function(pk) {
+		locsum += gameObject.players[pk].location;
+	});
+
+	if (locsum > 0) {
+		players.forEach(function(pk) {
+			if (loc == gameObject.players[pk].location) {
+				candidates.push(pk);
+			}
+		});
+		if (candidates.length > 1) {
+			console.log("-- Casos Beli --");
+			event.players = candidates;
+		}
+	}
+
+	return event;
 };
 
 app.get('/', function(req, res) {
@@ -30,12 +58,35 @@ io.on('connection', function(socket) {
 
 	socket.on('register', function(code) {
 		console.log('register: ', code);
-		global[code] = global[code] || {turn:""};
+		global[code] = global[code] || {
+			turn : "",
+			players : {},
+			event : {},
+			tags:[]
+		};
 		socket.on(code, function(msg) {
 			console.log(msg);
-			global[code] = merge.recursive(true, global[code], msg);
-			console.log(code, global[code]);
-			global[code]["turn"] = calculateNextTurn(global[code]);
+			
+			if(msg.event == "tags"){
+				global[code].tags = msg.tags; 
+			}else if (msg.event == "done") {
+				global[code].event.players.splice(global[code].event.players.indexOf(msg.player), 1);
+			} else {
+				global[code].players = merge.recursive(true, global[code].players, msg);
+				global[code].event = calculateEvent(global[code], msg);
+			}
+
+			if (global[code].event.players.length == 0) {
+				if(global[code].turn === ""){
+					global[code].turn = global[code].last_turn;
+					global[code].last_turn="";
+				}
+				global[code].turn = calculateNextTurn(global[code]);
+			} else {
+				global[code].last_turn = global[code].turn;
+				global[code].turn = "";
+			}
+
 			console.log(code, global[code]);
 			io.emit(code, global[code]);
 		});
@@ -46,4 +97,4 @@ io.on('connection', function(socket) {
 
 http.listen(4000, function() {
 	console.log('listening on *:4000');
-}); 
+});
